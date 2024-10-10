f35a5bafeb05ff6700a71b4593d4f53e9a910ab4
CleitonSocodolski <cleitonj198905@gmail.com>
2024-10-10 16:57:00 -0300

    commit parcial

--- wallet/internal/cmd/wallet.go
+++ wallet/internal/cmd/wallet.go
@@ -60,7 +60,7 @@ func (s *startWalletCmd) StartWallet() {
 
 func (s *startWalletCmd) configDB() string {
 	urlDb := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"localhost", 5432, "root", "wallettest", "postgres")
+		"host.docker.internal", 5432, "root", "wallettest", "postgres")
 
 	log.Println(urlDb)
 	return urlDb
